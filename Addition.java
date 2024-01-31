3a8baf12638ede3985485a8b3e4bc16a9432c3bc
unknown <purnanandagorantla@gmail.com>
2024-01-31 12:46:42 -0500

    added addition program

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Addition {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println("Addition num1 and num2 "+c);
+    }
+}
